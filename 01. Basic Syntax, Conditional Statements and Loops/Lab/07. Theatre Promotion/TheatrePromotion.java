1863f11573a10d50e4d4945f9622689ee32eb11f
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-06-02 01:43:25 +0300

    Create TheatrePromotion.java

---
+++ 01. Basic Syntax, Conditional Statements and Loops/Lab/07. Theatre Promotion/TheatrePromotion.java
@@ -0,0 +1,48 @@
+package BasicSyntaxConditionalStatementsAndLoops.Lab;
+
+import java.util.Scanner;
+
+public class TheatrePromotion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String day = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double ticketPrice = 0;
+
+        switch (day) {
+            case "Weekday":
+                if (age >= 0 && age <= 18) {
+                    ticketPrice = 12;
+                } else if (age > 18 && age <= 64) {
+                    ticketPrice = 18;
+                } else if (age > 64 && age <= 122) {
+                    ticketPrice = 12;
+                }
+                break;
+
+            case "Weekend":
+                if (age >= 0 && age <= 18) {
+                    ticketPrice = 15;
+                } else if (age > 18 && age <= 64) {
+                    ticketPrice = 20;
+                } else if (age > 64 && age <= 122) {
+                    ticketPrice = 15;
+                }
+                break;
+
+            case "Holiday":
+                if (age >= 0 && age <= 18) {
+                    ticketPrice = 5;
+                } else if (age > 18 && age <= 64) {
+                    ticketPrice = 12;
+                } else if (age > 64 && age <= 122) {
+                    ticketPrice = 10;
+                }
+        }
+        if (ticketPrice != 0) {
+            System.out.printf("%.0f$", ticketPrice);
+        } else {
+            System.out.println("Error!");
+        }
+    }
+}
