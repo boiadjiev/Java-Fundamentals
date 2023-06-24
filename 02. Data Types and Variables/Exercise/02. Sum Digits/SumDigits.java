09b200b6541ca176b89e97cfcd685e4ca86c0fca
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-06-25 00:44:12 +0300

    Create SumDigits.java

---
+++ 02. Data Types and Variables/Exercise/02. Sum Digits/SumDigits.java
@@ -0,0 +1,17 @@
+package DataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
+}
