27dde8f2f9353ba1de13bac29e1a1bcecb5b5167
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-07-25 11:39:48 +0300

    Create Calculations.java

---
+++ 04. Methods/Lab/04. Calculations /Calculations.java
@@ -0,0 +1,42 @@
+package Methods.Lab;
+
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        switch (command) {
+            case "add":
+                add(a, b);
+                break;
+            case "multiply":
+                multiply(a, b);
+                break;
+            case "subtract":
+                subtract(a, b);
+                break;
+            case "divide":
+                divide(a, b);
+        }
+    }
+
+    private static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    private static void multiply(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    private static void subtract(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    private static void divide(int a, int b) {
+        System.out.println((a / b));
+    }
+}
